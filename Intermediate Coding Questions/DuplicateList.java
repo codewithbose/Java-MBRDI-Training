c04f07b3b5bc41fadc883246a98729a5a292f57e
Katabathuni Bose <53533409+codewithbose@users.noreply.github.com>
2024-02-16 06:29:09 +0000

    duplicates update

--- Intermediate Coding Questions/DuplicateList.java
+++ Intermediate Coding Questions/DuplicateList.java
@@ -1,5 +1,6 @@
 import java.util.List;
 import java.util.Set;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.stream.Collectors;
 
@@ -9,6 +10,16 @@ public class DuplicateList {
         Set<Integer> seen = new HashSet<>();
         return numbers.stream()
                       .filter(n -> !seen.add(n)) // Add to set and check if already exists
-                      .collect(Collectors.toList());
+                      .collect(Collectors.toList());  
+    }
+    public static List<Integer> findNonDuplicatesUsingSet(List<Integer> numbers){
+        Set<Integer> notseen = new HashSet<>();
+        return numbers.stream().filter(n-> notseen.add(n))
+                    .collect(Collectors.toList());
+    }
+    public static void main(String[] args) {
+        List<Integer> allList = Arrays.asList(2, 3, 4, 5, 6, 4, 5);
+        System.out.println(findDuplicatesUsingSet(allList));
+        System.out.println(findNonDuplicatesUsingSet(allList));
     }
 }
